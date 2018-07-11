67a7183531c23ed65f8cf041c838c57d1cc7bd99
john kosinski <j.r.kosinski@gmail.com>
2018-07-11 13:27:15 +0700

    comments

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const await = require('asyncawait/await');
 const BLT = require('bitcoin-live-transactions');
 const bitcore = require('bitcore-lib'); 
 
+//TODO: 
+// - generate on-the-fly address
+// - listen for number of confirmations 
+// - transfer back into main wallet when done 
+
 module.exports = {
     Payment: require('./classes/Payment')
 }; 
